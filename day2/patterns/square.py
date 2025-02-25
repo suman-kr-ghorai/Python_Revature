a0ba0b0176026bef7c3fb60d27e3e83c7915501d
suman-k-ghorai <suman.kr.ghorai@gmail.com>
2025-02-25 15:46:21 +0530

    square pattern added

--- day2/patterns/square.py
+++ day2/patterns/square.py
@@ -1,1 +1,12 @@
+b=5
+l=5
+for i in range (l):
+    for j in range (b):
+        if i==0 or i==l-1 :
+            print('*', end="")
+        elif j==0 or j==b-1:
+            print('*', end="")
+        else:
+            print(' ', end="")
+    print('')
 
